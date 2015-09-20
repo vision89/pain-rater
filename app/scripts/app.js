201c9bc9dfa203f266b406a4bb35f757ac75d1e5
dlgulley411@gmail.com <dlgulley411@gmail.com>
2015-09-19 23:27:12 -0400

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,110 @@
+/*
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+(function(document) {
+  'use strict';
+
+  // Grab a reference to our auto-binding template
+  // and give it some initial binding values
+  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+  var app = document.querySelector('#app');
+
+
+  // See https://github.com/Polymer/polymer/issues/1381
+  window.addEventListener('WebComponentsReady', function() {
+    // imports are loaded and elements have been registered
+    
+    /**
+     * login button clicked
+     * @return {[type]} [description]
+     */
+    app.loginClicked = function () {
+
+      //Login page
+      page.redirect( '/login' );
+
+    };
+
+    /**
+     * logout button clicked
+     * @return {[type]} [description]
+     */
+    app.logoutClicked = function () {
+
+    };
+
+    app.goBack = function () {
+
+      page.redirect( app.goBackRoute );
+
+    };
+
+    /**
+     * Google Logged in
+     * @return {[type]} [description]
+     */
+    app.googleLoggedIn = function () {
+
+      page.redirect( '/' );
+
+    };
+
+    /**
+     * Google Logged out
+     * @return {[type]} [description]
+     */
+    app.facebookLoggedOut = function () {
+
+      page.redirect( '/' );
+
+    };
+
+    /**
+     * Facebook Logged in
+     * @return {[type]} [description]
+     */
+    app.facebookLoggedIn = function () {
+
+      page.redirect( '/' );
+
+    };
+
+    /**
+     * Facebook Logged out
+     * @return {[type]} [description]
+     */
+    app.facebookLoggedOut = function () {
+
+      page.redirect( '/' );
+
+    };
+
+    /**
+     * Twitter Logged in
+     * @return {[type]} [description]
+     */
+    app.twitterLoggedIn = function () {
+
+      page.redirect( '/' );
+
+    };
+
+    /**
+     * Twitter Logged out
+     * @return {[type]} [description]
+     */
+    app.twitterLoggedOut = function () {
+
+      page.redirect( '/' );
+
+    };
+
+  });
+
+})(document);
